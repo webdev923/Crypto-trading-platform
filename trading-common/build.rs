29180ed60c594f5b976c7d35463c0a3117a4dcd2
webdev923 <laijingan923@gmail.com>
2025-01-10 16:01:17 -0500

    fixed build script

--- trading-common/build.rs
+++ trading-common/build.rs
@@ -1,7 +1,10 @@
-use std::path::PathBuf;
+use std::{fs, path::PathBuf};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let out = PathBuf::from("src/generated");
+
+    // Create the generated directory if it doesn't exist
+    fs::create_dir_all(&out)?;
 
     println!("cargo:rerun-if-changed=proto/wallet.proto");
 
